e3f8424bc27c68a1860a83b75bd9a0e697b7e151
cybersekh <carolina.jpeg@gmail.com>
2023-01-04 13:40:30 -0300

    add conditional rendering

--- timer/src/App.jsx
+++ timer/src/App.jsx
@@ -1,21 +1,32 @@
 import { useState } from 'react'
 import TimerHimeno from './TimerHimeno'
+import TimerPower from './TimerPower'
+import TimerMakima from './TimerMakima'
+import TimerDenji from './TimerDenji'
 import './App.css'
 
 function App() {
+  const [timer, setTimer] = useState(null)
 
   return (
-    <main>
       <div className="container">
         <div className="buttons-container">
           <h3>Quantos minutos de comercial?</h3>
-          <button className="button-setup">04</button>
-          <button className="button-setup">06</button>
-          <button className="button-setup">08</button>
-          <button className="button-setup">10</button>
+          <button onClick={() => setTimer('himeno')} className="button-setup">04</button>
+          <button onClick={() => setTimer('power')} className="button-setup">06</button>
+          <button onClick={() => setTimer('denji')} className="button-setup">08</button>
+          <button onClick={() => setTimer('makima')} className="button-setup">10</button>
+        </div>
+        <div className="timers">
+          {timer === 'denji' && <TimerDenji />}
+          {timer === 'himeno' && <TimerHimeno />}
+          {timer === 'makima' && <TimerMakima />}
+          {timer === 'power' && <TimerPower />}
+        </div>
+        <div>
+          {timer === null && <img src="https://thumbs.gfycat.com/AliveAssuredGander-size_restricted.gif" />}
         </div>
       </div>
-    </main>
   )
 }
 
--- timer/src/TimerHimeno.jsx
+++ timer/src/TimerHimeno.jsx
@@ -17,10 +17,13 @@ function TimerHimeno() {
     }, [totalTimeInSeconds])
 
     return (
-        <div>
+        <div className="timerContainer">
+            <img className="imgHimeno" src="https://64.media.tumblr.com/81f2203f2651d72abe407717db40a7f2/f8c7b85690179dc9-66/s640x960/a1237d443072f1d7b63319146b880edf3d5809fb.gif" />
+            <div className="contador">
             <span>{minutes.toString().padStart(2, "0")}</span>
             <span>:</span>
             <span>{seconds.toString().padStart(2, "0")}</span>
+            </div>
         </div>
     )
 }
